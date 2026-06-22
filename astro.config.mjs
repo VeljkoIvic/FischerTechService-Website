e3e73f8d591fc4a7d4be9753c64897cd9a65896a
Ivic Veljko <ivic-veljko@hotmail.ch>
2026-06-22 21:12:57 +0200

    Refactor: Update navigation links and remove about page; implement redirects for improved user experience

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects: {
+    '/about': '/#services',
+  },
   vite: {
     css: {
       preprocessorOptions: {
